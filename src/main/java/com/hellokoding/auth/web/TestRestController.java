0a55fd10e2cec30077f6f43d737109e9b2fe8378
jinwen.lee <alvinlee001@live.com.my>
2019-06-11 09:50:24 +0800

    allow cross origin

--- src/main/java/com/hellokoding/auth/web/TestRestController.java
+++ src/main/java/com/hellokoding/auth/web/TestRestController.java
@@ -11,6 +11,7 @@
 
 package com.hellokoding.auth.web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TestRestController {
 
     @GetMapping("/test/endpoint")
